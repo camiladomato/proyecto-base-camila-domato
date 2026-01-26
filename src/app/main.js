22c74d4f7ae93fb0f2cc83047c59cadd56f6594b
camila domato <camiladomato@gmail.com>
2026-01-26 15:42:16 -0300

    ADD funcion para el buscador de chats

--- src/app/main.js
+++ src/app/main.js
@@ -1,10 +1,25 @@
 
 let $buscador = document.getElementById("search");
+let $chats = document.querySelectorAll(".user-chat");
 console.log($buscador);
+console.log($chats);
 
 const searchChats = () => {
-    console.log("buscando usuario")
+   const nameSearch = $buscador.value.toLowerCase();
+    
+   for (let i = 0 ; i < $chats.length; i++){
+    const $chat = $chats[i]
+    const $titleChat = $chat.querySelector("h3");
+    const name = $titleChat.textContent.toLowerCase();
+    
+    if(name.includes(nameSearch)){
+        $chat.style.display="flex";
+    }else{
+        $chat.style.display= "none";
+    }
+   }
+
 }
 
-$buscador.addEventListener("input", searchChats)
+$buscador.addEventListener("input", searchChats);
 
