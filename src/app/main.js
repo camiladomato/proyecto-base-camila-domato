952a2ef836733d9e168c2bec34141ae74f1f747d
camila domato <camiladomato@gmail.com>
2026-01-26 00:30:58 -0300

    ADD carpetas SRC, STYLES  y JS , ADD main js

---
+++ src/app/main.js
@@ -0,0 +1,10 @@
+
+let $buscador = document.getElementById("search");
+console.log($buscador);
+
+const searchChats = () => {
+    console.log("buscando usuario")
+}
+
+$buscador.addEventListener("input", searchChats)
+
