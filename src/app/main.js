9ac37d953ccea992b21d40717487c61b18e3646f
camila domato <camiladomato@gmail.com>
2026-01-27 00:59:11 -0300

    ADD agregando chats dinamicos con una simulacion de base de datos y renderizandolos con JS

--- src/app/main.js
+++ src/app/main.js
@@ -1,17 +1,49 @@
 
 let $buscador = document.getElementById("search");
-let $chats = document.querySelectorAll(".user-chat");
-console.log($buscador);
-console.log($chats);
+let $listChat = document.querySelector(".chats ul")
+
+
+const chatsData = [
+    {   name: "Damián Benitez",
+        lastMessage: "Vamos al cine ?",
+        avatar: "./assets/img/dami.jpg",
+    },
+    {   name: "Grupo UTN - turno mañana",
+        lastMessage: "que hicieron hoy?",
+        avatar: "./assets/img/muichiro.jpg",
+    },
+    {   name: "Movistar",
+        lastMessage: "Movistar te acerca la factura",
+        avatar: "./assets/img/mv.png",
+    }
+]
+
+const renderChats = () => {
+$listChat.innerHTML += ""
+
+chatsData.forEach((chat) => {
+$listChat.innerHTML += `
+<li class="user-chat">
+            <img src="${chat.avatar}" alt="foto de perfil de ${chat.name}" />
+            <div class="chat-info">
+              <h3>${chat.name}</h3>
+              <p>${chat.lastMessage}</p>
+            </div>
+            <span class="time">14:12 p. m.</span>
+          </li>      
+`
+})
+}
 
 const searchChats = () => {
+    let $chats = document.querySelectorAll(".user-chat");
    const nameSearch = $buscador.value.toLowerCase();
     
    for (let i = 0 ; i < $chats.length; i++){
     const $chat = $chats[i]
     const $titleChat = $chat.querySelector("h3");
     const name = $titleChat.textContent.toLowerCase();
-    
+
     if(name.includes(nameSearch)){
         $chat.style.display="flex";
     }else{
@@ -22,4 +54,4 @@ const searchChats = () => {
 }
 
 $buscador.addEventListener("input", searchChats);
-
+renderChats()
